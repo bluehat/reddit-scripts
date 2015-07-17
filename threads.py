8143918401dce0b36003233bf3200c7408e920c2
Katy Levinson <bluehat@fake.com>
2015-07-17 01:53:57 -0700

    lol, time.sleep is in real seconds not milliseconds. Maybe I need some time.sleep

--- threads.py
+++ threads.py
@@ -6,7 +6,6 @@ import csv
 import socket
 socket.setdefaulttimeout(30)
 
-results = []
 base_url = ""
 
 fields = [
@@ -20,33 +19,37 @@ threads = csv.writer(threadfile)
 
 print(','.join(fields))
  
-def get_next_batch(after=''):
-	global results
-	response = urllib.request.urlopen(base_url+"&after={}".format(after)).read();
-	data  = json.loads(response.decode())
-	hits  = data['data']['children']
-	after = data['data']['after']
-	for hit in hits:
-		hit = hit['data']
-		stamp = datetime.datetime.utcfromtimestamp(hit['created_utc']).timetuple()
-		hit['created_utc'] = time.strftime('%Y-%m-%d', stamp)
-		row = {}
-		for k in fields:
-			row[k] = hit[k]
-		results = ""
-		for r in row:
-			results = results + str(row[r]) + " "
-		threads.writerow(results.split())
-	get_next_batch(after)
-	sleep(10000)
+def get_next_batch():
+	after = ''
+	while after is not None:
+		response = urllib.request.urlopen(base_url+"&after={}".format(after)).read();
+		data  = json.loads(response.decode())
+		hits  = data['data']['children']
+		after = data['data']['after']
+		print(after)
+		for hit in hits:
+			hit = hit['data']
+			stamp = datetime.datetime.utcfromtimestamp(hit['created_utc']).timetuple()
+			hit['created_utc'] = time.strftime('%Y-%m-%d', stamp)
+			row = {}
+			for k in fields:
+				row[k] = hit[k]
+			results = ""
+			for r in row:
+				results = results + str(row[r]) + " "
+			threads.writerow(results.split())
+		time.sleep(2)
+	print('peace bitches')
  
-subreddits = csv.reader(open("subreddits.csv"))
+subreddits = csv.reader(open('subreddits.csv'))
+# for row in subreddits:
+# 	print(row[0])
 for row in subreddits:
-	base_url = "http://www.reddit.com"+ row[0] +".json?"
-	print("Subreddit " + row[0])
+	base_url = 'http://www.reddit.com'+ row[0] +'.json?'
+	print('Subreddit: ' + row[0])
 	try:
-		get_next_batch('t5_38lwr')
-		sleep(10000)
+		get_next_batch()
+		time.sleep(10)
 	except:
 		threadfile.close()
 		raise
